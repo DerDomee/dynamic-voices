54908802fa3cbd5f04605b9fd6cd78317ebb378d
Dominik Riedig <me@dominikriedig.de>
2021-12-23 22:27:34 +0100

    Better code readability

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,15 @@
 const logger = require('./logger').logger;
 const ddlib = require('./ddlib');
 const { Client, Intents, Collection } = require('discord.js');
-const client = new Client({ intents: [
-	Intents.FLAGS.GUILDS,
-	Intents.FLAGS.GUILD_MEMBERS,
-	Intents.FLAGS.GUILD_MESSAGES,
-	Intents.FLAGS.GUILD_VOICE_STATES] });
+const client = new Client({
+	intents:
+		[
+			Intents.FLAGS.GUILDS,
+			Intents.FLAGS.GUILD_MEMBERS,
+			Intents.FLAGS.GUILD_MESSAGES,
+			Intents.FLAGS.GUILD_VOICE_STATES,
+		],
+});
 
 const slashCommands = ddlib.loadSlashCommands();
 
