09c7c3534193fef7d49c2a7d3c3e4245bfdb4845
Dominik Riedig <me@dominikriedig.de>
2023-04-11 17:51:16 +0200

    style: refactor for better code readability

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,27 +16,25 @@ module.exports = {
 		'@typescript-eslint',
 	],
 	'rules': {
-		'comma-dangle': ['error', 'always-multiline',
-		],
-		'comma-spacing': ['error',
+		'comma-dangle': ['error', 'always-multiline'],
+		'comma-spacing': [
+			'error',
 			{
 				before: false, after: true,
 			},
 		],
-		'comma-style': ['error', 'last',
-		],
-		'indent': ['error', 'tab',
-		],
-		'new-cap': ['error',
+		'comma-style': ['error', 'last'],
+		'indent': ['error', 'tab'],
+		'new-cap': [
+			'error',
 			{
 				properties: false,
 			},
 		],
 		'no-tabs': 0,
-		'no-mixed-spaces-and-tabs': [
-			2, 'smart-tabs',
-		],
-		'new-cap': ['error',
+		'no-mixed-spaces-and-tabs': [2, 'smart-tabs'],
+		'new-cap': [
+			'error',
 			{
 				'capIsNew': false,
 			},
